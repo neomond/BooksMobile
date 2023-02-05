83f99ae647afc4f79e1d562392a9d20589047520
neomond <neoweise@yahoo.com>
2023-02-05 14:13:02 +0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import MyBooksProvider from "./context/MyBooksProvider";
+
+const API_KEY =
+  "apollobeach::stepzen.net+1000::0d1bfb4dc714f02ffe39127321bc5de1f3e538006cff95aefcbbce48d0c95b4a";
+
+const client = new ApolloClient({
+  uri: "https://apollobeach.stepzen.net/api/ungaged-porcupine/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,8 +27,12 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <ApolloProvider client={client}>
+          <MyBooksProvider>
+            <Navigation colorScheme={colorScheme} />
+          </MyBooksProvider>
+          <StatusBar />
+        </ApolloProvider>
       </SafeAreaProvider>
     );
   }
